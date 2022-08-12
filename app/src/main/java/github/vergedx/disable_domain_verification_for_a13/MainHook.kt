60caaa7ded51177599a8bab93996c781e89990c6
Vanilla <osu_Vanilla@126.com>
2022-08-12 13:51:38 +0800

    style[MainHook.kt]: Migrant codes to Kotlin.

---
+++ app/src/main/java/github/vergedx/disable_domain_verification_for_a13/MainHook.kt
@@ -0,0 +1,24 @@
+package github.vergedx.disable_domain_verification_for_a13
+
+import android.content.Intent
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.XC_MethodReplacement
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
+
+class MainHook : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: LoadPackageParam) {
+        if (lpparam.packageName != "android") return
+
+        XposedHelpers.findAndHookMethod(
+            // https://developer.android.com/reference/android/content/pm/PackageManager.ResolveInfoFlags
+            "com.android.server.pm.verify.domain.DomainVerificationUtils", lpparam.classLoader,
+            "isDomainVerificationIntent", Intent::class.java, Long::class.javaPrimitiveType,
+            object : XC_MethodReplacement() {
+                override fun replaceHookedMethod(param: MethodHookParam): Any {
+                    return false
+                }
+            }
+        )
+    }
+}
